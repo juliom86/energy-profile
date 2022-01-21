377e2aa07296c4762085e049991f7381ecb94412
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2022-01-20 21:11:21 -0300

    add the initial graphs to test streamlit and setup the pilars to connect the API

--- application/main.py
+++ application/main.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import time
+# from application.functions import convert_df
 
 def run():
     api_request_progress_bar = st.progress(0)
-    col1, col2, col3 = st.columns(3)
+    st.title("Producción vs. consumo de energía")
+    col1, col2 = st.columns([3, 1])
+    col3, col4 = st.columns([3, 1])
 
     if st.sidebar.button('Solicitar información'):
         for percent_complete in range(100):
@@ -12,23 +18,36 @@ def run():
 
         api_request_progress_bar.empty()
 
-        with col1:
-            st.header("Grafico 1")
-            st.write('Seleccionado')
+        df_gen_vs_con = pd.read_csv('datasets/gen_vs_con.csv')
+
+        col1.subheader("Generación Historica nivel pais en Argentina")
+
+        generation_fig = px.line(
+            df_gen_vs_con,
+            x='Fecha',
+            y='Generación GWh',
+            width=1100,
+            height=600)
+
+        col1.plotly_chart(generation_fig)
 
         with col2:
-            st.header("Grafico 1")
-            st.write('Seleccionado')
+            with st.expander("Mayor información y descarga de datos."):
+                st.write("""
+                    The chart above shows some numbers I picked for you.
+                    I rolled actual dice for these, so they're *guaranteed* to
+                    be random.
+                """)
 
-        with col3:
-            st.header("Grafico 1")
-            st.write('Seleccionado')
-            # st.download_button(
-            #     label="Download data as CSV",
-            #     data={'data': convert_df(df)},
-            #     file_name='large_df.csv',
-            #     mime='text/csv',
-            # )
+        col3.subheader("Consumo histórica nivel pais en Argentina")
+        consumption_fig = px.line(
+            df_gen_vs_con,
+            x='Fecha',
+            y='Consumo GWh',
+            width=1100,
+            height=600)
+
+        col3.plotly_chart(consumption_fig)
 
     else:
         st.write('')
