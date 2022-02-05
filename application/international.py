fecf0f4ed4992f7badfd5aca15ccc507ea8ff278
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2022-02-05 10:47:36 -0300

    update graph size and add download/API documentation

--- application/international.py
+++ application/international.py
@@ -5,6 +5,10 @@ import plotly.graph_objects as go
 import requests
 import os
 
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(index=False).encode('utf-8')
+
 
 def run():
     env = os.getenv('FLASK_ENV')
@@ -19,26 +23,28 @@ def run():
     col3, col4 = st.columns([4, 2])
 
     # International Tab
-    import_export = pd.read_csv(
+    import_export_df = pd.read_csv(
         'datasets/Exportaciones e importaciones historicas.csv', sep=';')
 
     ## Exportation
-    fig_export = px.bar(import_export,
+    fig_export = px.bar(import_export_df,
                  x="Año",
                  color="pais",
                  y='Exportación',
                  title="Exportaciones de energía desde Argentina",
                  barmode='group',
-                 height=600)
+                 height=600,
+                 width=1100)
 
     ## Importation
-    fig_import = px.bar(import_export,
-                 x="Año",
-                 color="pais",
-                 y='Importación',
-                 title="Importación de energía de Argentina",
-                 barmode='group',
-                 height=600)
+    fig_import = px.bar(import_export_df,
+                        x="Año",
+                        color="pais",
+                        y='Importación',
+                        title="Importación de energía de Argentina",
+                        barmode='group',
+                        height=600,
+                        width=1100)
 
     col1.plotly_chart(fig_export)
 
@@ -47,6 +53,25 @@ def run():
             #### Exportaciones
             Si existen excedentes de energía eléctrica generada se exportan a países vecinos. .
         """)
+        st.write("**Fuente:** [XXXXXXXXX](https://www.lewagon.com)")
+        export_csv = convert_df(import_export_df[['Año', 'pais', 'Exportación']])
+        st.download_button(label="Descargar dataset en CSV",
+                           data=export_csv,
+                           file_name='exportación_por_pais.csv',
+                           mime='text/csv',
+                           key="1")
+        st.write("""
+                 ---
+                 """)
+        st.write("**API Endpoint**")
+        st.write("""
+                 Puedes realizar el request a `/balance` con los siguientes paramtros obligatorios:
+                 - `start_year`: entero y desde 2005 a 2019
+                 - `end_year`: entero y desde 2005 a 2019
+                 - `kpi`: `export`
+
+                 👉 [Ver documentación completa](https://documenter.getpostman.com/view/5438737/UVXnGENw)
+                 """)
 
     col3.plotly_chart(fig_import)
 
@@ -55,3 +80,23 @@ def run():
             #### Importaciones
             Se importa energía eléctrica de otros países cuando los de los centros de producción de energía están muy alejados.
         """)
+        st.write("**Fuente:** [XXXXXXXXX](https://www.lewagon.com)")
+        exportacion_csv = convert_df(
+            import_export_df[['Año', 'pais', 'Importación']])
+        st.download_button(label="Descargar dataset en CSV",
+                           data=exportacion_csv,
+                           file_name='importacion_por_pais.csv',
+                           mime='text/csv',
+                           key="1")
+        st.write("""
+                 ---
+                 """)
+        st.write("**API Endpoint**")
+        st.write("""
+                 Puedes realizar el request a `/balance` con los siguientes paramtros obligatorios:
+                 - `start_year`: entero y desde 2005 a 2019
+                 - `end_year`: entero y desde 2005 a 2019
+                 - `kpi`: `import`
+
+                 👉 [Ver documentación completa](https://documenter.getpostman.com/view/5438737/UVXnGENw)
+                 """)
